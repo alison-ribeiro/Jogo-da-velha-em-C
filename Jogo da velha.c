be121a2d37b1f3fead8bde05b7a86e4f4bc7e2ef
Alison R Alves <alisonribeiro075@gmail.com>
2022-12-20 18:41:48 -0300

    Organização do codigo

--- Jogo da velha.c
+++ Jogo da velha.c
@@ -27,10 +27,10 @@ int ganhouPorLinha(int l, char c){
 		return 0;
 }
 
-int ganhouPorLinhas(char teste){
+int ganhouPorLinhas(){
 	int ganhou = 0;
 	for (i = 0; i < n; i++)	{
-		ganhou +=	ganhouPorLinha(i, teste);
+		ganhou +=	ganhouPorLinha(i, jogador);
 	}
 	return ganhou;
 }
@@ -66,52 +66,28 @@ int ganhouPorDiagonalSec(char c){
 		return 0;
 }
 
-
-/*
-		0   1   2
-		|   |   |
-0	-	7 | 8 | 9
-		---------
-1	-	4 | 5 | 6
-		---------
-2	-	1 | 2 | 3
-
-*/
-
 void vezJogador(){
 	if(jogador == 'X')
 		jogador = 'O';
 	else
 		jogador = 'X';	
 }
+
 void ehValida(int l, int c){
 	if(jogo[l][c] == ' '){
 		jogo[l][c] = jogador;
 		
 	}else{
-		printf("\nCaiu aqui\n");
 		jogadas--;
 		vezJogador();
 	}
 
-	v += ganhouPorLinhas(jogador);
+	v += ganhouPorLinhas();
 	v += ganhouPorColunas();
 	v += ganhouPorDiagonalPrim(jogador);
 	v += ganhouPorDiagonalSec(jogador);
 
 }
-
-/*
-	int testess(){
-		int q = 0;
-		q += ganhouPorLinhas(jogador);
-		q += ganhouPorColunas();
-		q += ganhouPorDiagonalPrim(jogador);
-		q += ganhouPorDiagonalSec(jogador); 
-
-		return q;
-	}
-*/
 
 
 void jogadaCasa(){
@@ -149,21 +125,18 @@ void jogadaCasa(){
 }
 
 
-
 void renderizarTabuleiro(char tabuleiro[n][n]){
 	
 	for (i = 0; i < n; i++){
-
 		for (j = 0; j < n; j++){
-
 			if(j == 1 )
+			
 				printf("| %c |", tabuleiro[i][j]);
 			else
 				printf(" %c ", tabuleiro[i][j]);
-
 		}
-
 		if(i == 2){
+
 		}
 		else
 			printf("\n----------\n");
@@ -171,72 +144,91 @@ void renderizarTabuleiro(char tabuleiro[n][n]){
 	
 }
 
+
+void iniciar(int m){
+	int menu = m;
+	v = 0;
+	jogadas = 0;
+
+	if(menu == 2){
+		printf("      #---------------------# \n");
+		printf("      # digite 1 para jogar #     \n");
+		printf("      #---------------------# \n\n");
+
+		printf("      #--------------------# \n");
+		printf("      # digite 0 para sair #\n");
+		printf("      #--------------------# \n");
+
+		scanf("%d", &menu);
+	}
+	if(menu == 0){
+		system("cls");
+		printf("Encerranco jogo .....\n\n");
+		exit(0); 
+	}
+	if(menu == 1){
+		system("cls");
+		printf(" |=============|\n");
+		printf(" |Jogo da velha|\n ");
+		printf("|=============|  ");
+
+		inicializarMatriz();
+		printf("\n\n");
+		renderizarTabuleiro(exemplo);
+
+		printf("\n\n");
+		menu = 3;
+	}
+}
 int main() {
 	
-	
-
-	printf(" |=============|\n");
-	printf(" |Jogo da velha|\n ");
-	printf("|=============|  ");
-	inicializarMatriz();
-	printf("\n\n");
-
-	renderizarTabuleiro(exemplo);
-
-	printf("\n\n");
+	iniciar(2);
  
 	do{
-
 		jogadas++;
-
-		printf("Jogador da vez %c \n", jogador);
+		printf("      #------------------# \n");
+		printf("      # Jogador da vez %c #\n ", jogador);
+		printf("     #------------------# \n\n");
 		printf("Escolha uma casa: ");
 		scanf("%d", &casa);
 
 		while (casa < 0 || casa > 9){
 			jogadas--;
-			printf("Jogador da vez %c \n", jogador);
+			printf("      #----------------------# \n");
+			printf("      # Jogador da vez %c \n #", jogador);
+			printf("      #----------------------# \n\n");
 			printf("Casa invalida!! \n ");
 			printf("Escolha uma casa valida: ");
 			scanf("%d", &casa);
 			jogadas++;
 		}
 
-		
-
 		printf("\n");
 		jogadaCasa();
 		renderizarTabuleiro(jogo);
-		
-
 		printf("\n");
-		
-		
-
-		
 
 		if(v > 0 ){
-			printf("\nJogador %c ganhou \n", jogador);
-		} 
+			printf("      $$$$$$$$$$$$$$$$$$$$$$$$ \n");
+			printf("      $ Jogador %c ganhou    $ \n", jogador);
+			printf("      $$$$$$$$$$$$$$$$$$$$$$$$ \n");
+			iniciar(2);
 
-		printf("\n\njogada = %d e V = %d \n\n", jogadas, v);
+		} 
 		if(jogadas == 10 && v == 0){
 			v = 1;
-			printf("Jogo empatou");
-		}
+			printf("      $$$$$$$$$$$$$$$$$$$$$$$$ \n");
+			printf("      $      Jogo empatou    $ \n");
+			printf("      $$$$$$$$$$$$$$$$$$$$$$$$ \n");
+			iniciar(2);
 			
+		}
+
 		vezJogador();
+
 	} while (v == 0);
 	
-
-	
-
 	printf("\n\n");
-
-
-
-
-
 
 	system("pause");
 	return 0;
